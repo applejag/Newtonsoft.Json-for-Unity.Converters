51f9bd554a59d9233500aafb4670bab7421582eb
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-01-19 20:36:37 +0100

    Removed Unity project and replaced with C# solution

---
+++ Src/Newtonsoft.Json.UnityConverters.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Newtonsoft.Json.UnityConverters.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
