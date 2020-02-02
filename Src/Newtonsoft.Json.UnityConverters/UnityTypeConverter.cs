608299f7abb0c6d3368256454d7eba3d49ee4bdb
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-02-02 11:16:26 +0100

    Added UnityConverters from @sindrijo's suggestion

---
+++ Src/Newtonsoft.Json.UnityConverters/UnityTypeConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+namespace Newtonsoft.Json.Converters.Unity {
+    public class UnityTypeConverter : JsonConverter
+    {
+        private static readonly HashSet<Type> UnityEngineTypes = new HashSet<Type>(typeof(UnityEngine.Object).Assembly.GetTypes());
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteRawValue(JsonUtility.ToJson(value));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return JsonUtility.FromJson(JObject.Load(reader).ToString(), objectType);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return IsUnityEngineType(objectType);
+        }
+
+        private static bool IsUnityEngineType(Type objectType)
+        {
+            return UnityEngineTypes.Contains(objectType);
+        }
+    }
+}
---
+++ Src/Newtonsoft.Json.UnityConverters/UnityTypeConverterInitializer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+namespace Newtonsoft.Json.Unity
+{
+    internal static class UnityTypeConverterInitializer
+    {
+
+#if UNITY_EDITOR
+        [UnityEditor.InitializeOnLoadMethod]
+#else
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+#endif
+#pragma warning disable IDE0051 // Remove unused private members
+        private static void Init()
+#pragma warning restore IDE0051 // Remove unused private members
+        {
+            JsonConvert.DefaultSettings += GetJsonSerializerSettings;
+        }
+
+        private static JsonSerializerSettings GetJsonSerializerSettings()
+        {
+            var settings = new JsonSerializerSettings();
+            settings.Converters.Add(new UnityTypeConverter());
+            return settings;
+        }
+
+        private class UnityTypeConverter : JsonConverter
+        {
+            private static readonly HashSet<Type> UnityEngineTypes = new HashSet<Type>(typeof(UnityEngine.Object).Assembly.GetTypes());
+
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                writer.WriteRawValue(JsonUtility.ToJson(value));
+            }
+
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            {
+                return JsonUtility.FromJson(JObject.Load(reader).ToString(), objectType);
+            }
+
+            public override bool CanConvert(Type objectType)
+            {
+                return IsUnityEngineType(objectType);
+            }
+
+            private static bool IsUnityEngineType(Type objectType)
+            {
+                return UnityEngineTypes.Contains(objectType);
+            }
+        }
+    }
+}
