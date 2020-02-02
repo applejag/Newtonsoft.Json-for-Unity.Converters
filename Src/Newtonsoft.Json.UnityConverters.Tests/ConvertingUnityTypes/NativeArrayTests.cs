49a668e203a91905b1e90d520329d71a9cf97b79
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-02-02 17:47:11 +0100

    Added NativeArray<> tests

---
+++ Src/Newtonsoft.Json.UnityConverters.Tests/ConvertingUnityTypes/NativeArrayTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+using Unity.Collections;
+
+namespace Newtonsoft.Json.UnityConverters.Tests.ConvertingUnityTypes
+{
+    public class NativeArrayTests : TypeTesterBase
+    {
+        public static (NativeArray<int> deserialized, string serialized)[] Representations { get; } = new[] {
+            (new NativeArray<int>(new int[]{ 1,2,3,4 }, Allocator.Persistent), @"[1,2,3,4]"),
+            (new NativeArray<int>(Array.Empty<int>(), Allocator.None), @"[]"),
+        };
+
+        protected override void ConfigureSettings(JsonSerializerSettings settings)
+        {
+        }
+
+        [Test]
+        [TestCaseSource("Representations")]
+        public void SerializesAsExpected((NativeArray<int> input, string expected) representation)
+        {
+            // Arrange
+            JsonSerializerSettings settings = GetSettings();
+
+            // Act
+            string result = JsonConvert.SerializeObject(representation.input, settings);
+
+            // Assert
+            Assert.AreEqual(representation.expected, result);
+        }
+
+        [Test]
+        [TestCaseSource("Representations")]
+        public void DeserializesAsExpected((NativeArray<int> expected, string input) representation)
+        {
+            // Arrange
+            JsonSerializerSettings settings = GetSettings();
+
+            // Act
+            NativeArray<int> result = JsonConvert.DeserializeObject<NativeArray<int>>(representation.input, settings);
+
+            // Assert
+            Assert.IsTrue(result.IsCreated);
+            CollectionAssert.AreEqual(representation.expected, result);
+        }
+    }
+}
