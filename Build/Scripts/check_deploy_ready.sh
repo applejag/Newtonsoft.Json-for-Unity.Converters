bad6df8eb14606c2242f5882dceab389a9bb4a08
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-03-15 19:41:20 +0100

    Moved check-deploy logic into it's own file

---
+++ Build/Scripts/check_deploy_ready.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Set error flags
+set -o nounset
+set -o errexit
+set -o pipefail
+
+OK=1
+
+if [ "$(npm search jillejr.newtonsoft.json-for-unity.converters)" == 'No matches found for "jillejr.newtonsoft.json-for-unity.converters"' ]
+then
+    echo "> Package jillejr.newtonsoft.json-for-unity.converters does not exist on the registry and is available for publishing, all ok!"
+elif [ -z "$(npm view jillejr.newtonsoft.json-for-unity.converters@$VERSION versions)" ]
+then
+    echo "> Package jillejr.newtonsoft.json-for-unity.converters version $VERSION does not exist on the registry and is available for publishing, all ok!"
+else
+    echo
+    echo "[!] Package version $VERSION already existed on $NPM_REGISTRY"
+    echo "[!] Make sure to update the /Build/version.json"
+    OK=0
+fi
+
+echo
+
+if git tag --list | egrep -q "^$VERSION$"
+then
+    echo
+    echo "[!] Tag $VERSION already existed."
+    echo "[!] Make sure to update the /Build/version.json"
+    OK=0
+else
+    echo "> Tag $VERSION is available for publishing, all ok!"
+fi
+
+echo
+
+if [ "$VERSION_SUFFIX" != "" ]
+then
+    echo "> Ignoring to check changelog since suffix is '$VERSION_SUFFIX', all ok!"
+elif cat CHANGELOG.md | egrep -q "^## $VERSION$"
+then
+    echo "> Changelog has been updated, all ok!"
+else
+    echo
+    echo "[!] Changelog in CHANGELOG.md is missing line '## $VERSION'."
+    echo "[!] Make sure to update the CHANGELOG.md"
+    OK=0
+fi
+
+echo
+
+if [ $OK != 1 ]
+then
+    echo "At least one check failed. Aborting!"
+    exit 1
+fi
+
+echo "Nice work! Happy deploying!"
