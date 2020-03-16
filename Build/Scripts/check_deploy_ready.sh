fc3c9625ea28585fd35fc1f1317a4f8452dcdb1e
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-03-16 21:14:56 +0100

    Changed changelog to check CONVERTERS version instead of UPM

--- Build/Scripts/check_deploy_ready.sh
+++ Build/Scripts/check_deploy_ready.sh
@@ -5,8 +5,8 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-: ${VERSION:?"Need the version to be checked"}
-: ${VERSION_SUFFIX:?"Need the version suffix to be checked"}
+: ${VERSION_UPM:?"Need the version to be checked"}
+: ${VERSION_CONVERTERS:?"Need the version of Newtonsoft.Json.UnityConverters to be checked"}
 : ${NPM_REGISTRY:?"Need the NPM registry to be checked against"}
 
 OK=1
@@ -14,39 +14,36 @@ OK=1
 if [ "$(npm search jillejr.newtonsoft.json-for-unity.converters)" == 'No matches found for "jillejr.newtonsoft.json-for-unity.converters"' ]
 then
     echo "> Package jillejr.newtonsoft.json-for-unity.converters does not exist on the registry and is available for publishing, all ok!"
-elif [ -z "$(npm view jillejr.newtonsoft.json-for-unity.converters@$VERSION versions)" ]
+elif [ -z "$(npm view jillejr.newtonsoft.json-for-unity.converters@$VERSION_UPM versions)" ]
 then
-    echo "> Package jillejr.newtonsoft.json-for-unity.converters version $VERSION does not exist on the registry and is available for publishing, all ok!"
+    echo "> Package jillejr.newtonsoft.json-for-unity.converters version $VERSION_UPM does not exist on the registry and is available for publishing, all ok!"
 else
     echo
-    echo "[!] Package version $VERSION already existed on $NPM_REGISTRY"
+    echo "[!] Package version $VERSION_UPM already existed on $NPM_REGISTRY"
     echo "[!] Make sure to update the /Build/version.json"
     OK=0
 fi
 
 echo
 
-if git tag --list | egrep -q "^$VERSION$"
+if git tag --list | egrep -q "^$VERSION_UPM$"
 then
     echo
-    echo "[!] Tag $VERSION already existed."
+    echo "[!] Tag $VERSION_UPM already existed."
     echo "[!] Make sure to update the /Build/version.json"
     OK=0
 else
-    echo "> Tag $VERSION is available for publishing, all ok!"
+    echo "> Tag $VERSION_UPM is available for publishing, all ok!"
 fi
 
 echo
 
-if [ "$VERSION_SUFFIX" != "" ]
-then
-    echo "> Ignoring to check changelog since suffix is '$VERSION_SUFFIX', all ok!"
-elif egrep -f CHANGELOG.md -q "^## $VERSION$"
+if egrep -f CHANGELOG.md -q "^## ${VERSION_CONVERTERS//\./\\.}$"
 then
     echo "> Changelog has been updated, all ok!"
 else
     echo
-    echo "[!] Changelog in CHANGELOG.md is missing line '## $VERSION'."
+    echo "[!] Changelog in CHANGELOG.md is missing line '## $VERSION_CONVERTERS'."
     echo "[!] Make sure to update the CHANGELOG.md"
     OK=0
 fi
