9b4f7fed1080c5556b44b79a487c5432e08201df
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-04-23 18:43:07 +0200

    Added basic JsonSerializerConfig

---
+++ Src/Newtonsoft.Json.UnityConverters/JsonSerializerConfig.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Newtonsoft.Json.UnityConverters
+{
+    [CreateAssetMenu(fileName = "JsonSerializerConfig.asset", menuName = "Newtonsoft.Json Converters config")]
+    public class JsonSerializerConfig : ScriptableObject
+    {
+        public bool someBool1;
+        public bool someBool2;
+    }
+}
