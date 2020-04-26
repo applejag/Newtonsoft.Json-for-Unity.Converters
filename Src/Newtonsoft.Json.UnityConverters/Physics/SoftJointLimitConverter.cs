b338d5e86240bba41918adf2c8898db397d36107
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-04-26 16:56:56 +0200

    Added converter for SoftJointLimit

---
+++ Src/Newtonsoft.Json.UnityConverters/Physics/SoftJointLimitConverter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Newtonsoft.Json.UnityConverters
+{
+    public class SoftJointLimitConverter : PartialFloatConverter<SoftJointLimit>
+    {
+        private static readonly string[] _memberNames = { "limit", "bounciness", "contactDistance" };
+
+        public SoftJointLimitConverter()
+            : base(_memberNames)
+        {
+        }
+
+        protected override SoftJointLimit CreateInstanceFromValues(ValuesArray<float> values)
+        {
+            return new SoftJointLimit {
+                limit = values.GetAsTypeOrDefault<float>(0),
+                bounciness = values.GetAsTypeOrDefault<float>(1),
+                contactDistance = values.GetAsTypeOrDefault<float>(2),
+            };
+        }
+
+        protected override float[] ReadInstanceValues(SoftJointLimit instance)
+        {
+            return new[] {
+                instance.limit,
+                instance.bounciness,
+                instance.contactDistance,
+            };
+        }
+    }
+}
