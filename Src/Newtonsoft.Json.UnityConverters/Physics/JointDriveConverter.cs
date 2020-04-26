5abf901fc0db7c4e933c3264789c1343e834e7f1
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-04-26 16:53:17 +0200

    Added converter for JointDrive

---
+++ Src/Newtonsoft.Json.UnityConverters/Physics/JointDriveConverter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Newtonsoft.Json.UnityConverters
+{
+    public class JointDriveConverter : PartialFloatConverter<JointDrive>
+    {
+        private static readonly string[] _memberNames = { "positionSpring", "positionDamper", "maximumForce" };
+
+        public JointDriveConverter()
+            : base(_memberNames)
+        {
+        }
+
+        protected override JointDrive CreateInstanceFromValues(ValuesArray<float> values)
+        {
+            return new JointDrive {
+                positionSpring = values.GetAsTypeOrDefault<float>(0),
+                positionDamper = values.GetAsTypeOrDefault<float>(1),
+                maximumForce = values.GetAsTypeOrDefault<float>(2),
+            };
+        }
+
+        protected override float[] ReadInstanceValues(JointDrive instance)
+        {
+            return new[] {
+                instance.positionSpring,
+                instance.positionDamper,
+                instance.maximumForce,
+            };
+        }
+    }
+}
