0ed53803adf4325ce0fef78a38fa44f848345973
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-04-26 16:45:06 +0200

    Added converter for JointLimits

---
+++ Src/Newtonsoft.Json.UnityConverters/Physics/JointLimitsConverter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Newtonsoft.Json.UnityConverters
+{
+    public class JointLimitsConverter : PartialFloatConverter<JointLimits>
+    {
+        private static readonly string[] _memberNames = { "min", "max", "bounciness", "bounceMinVelocity", "contactDistance" };
+
+        public JointLimitsConverter()
+            : base(_memberNames)
+        {
+        }
+
+        protected override JointLimits CreateInstanceFromValues(ValuesArray<float> values)
+        {
+            return new JointLimits {
+                min = values.GetAsTypeOrDefault<float>(0),
+                max = values.GetAsTypeOrDefault<float>(1),
+                bounciness = values.GetAsTypeOrDefault<float>(2),
+                bounceMinVelocity = values.GetAsTypeOrDefault<float>(3),
+                contactDistance = values.GetAsTypeOrDefault<float>(4),
+            };
+        }
+
+        protected override float[] ReadInstanceValues(JointLimits instance)
+        {
+            return new[] {
+                instance.min,
+                instance.max,
+                instance.bounciness,
+                instance.bounceMinVelocity,
+                instance.contactDistance,
+            };
+        }
+    }
+}
