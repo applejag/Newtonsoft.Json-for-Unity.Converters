297995228c5565601795cc89428ec540a94a77dd
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-05-03 13:45:53 +0200

    Added deploy check for environment variables

--- Build/Scripts/check_deploy_ready.sh
+++ Build/Scripts/check_deploy_ready.sh
@@ -24,6 +24,21 @@ fi
 
 echo
 
+for ENV_VAR in "NPM_AUTH_TOKEN GIT_USER_EMAIL GIT_USER_NAME GIT_GPG_ID GIT_GPG_SEC_B64"
+do
+    if [ -z "${!ENV_VAR}" ]
+    then
+        echo "> Environment variable \$$ENV_VAR is set, all ok!"
+    else
+        echo
+        echo "[!] Missing environment variable \$$ENV_VAR."
+        OK=0
+    fi
+
+done
+
+echo
+
 if [ $OK != 1 ]
 then
     echo "At least one check failed. Aborting!"
