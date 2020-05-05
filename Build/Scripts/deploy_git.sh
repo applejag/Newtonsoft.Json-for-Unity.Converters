22598f0cecca469552e9f449366a77da0aa3d97d
Kalle Jillheden <kalle.jillheden@gmail.com>
2020-05-05 22:32:22 +0200

    Updated deploy_git.sh to deploy git variants as upm/$VERSION_UPM instead

--- Build/Scripts/deploy_git.sh
+++ Build/Scripts/deploy_git.sh
@@ -11,68 +11,97 @@ set -o pipefail
 : ${REPO_FOLDER:?}
 : ${PACKAGE_FOLDER:?}
 
-if git tag --list | egrep -q "^$VERSION_UPM$"
+TAG_UPM="upm/$VERSION_UPM"
+TAG_MASTER="$VERSION_UPM"
+
+tag_and_push() {
+    local tagName="$1"
+    local tagMessage="$2"
+
+    git tag $tagName -m "$tagMessage"
+
+    echo "Created tag: '$(git tag -l $tagName -n1))'"
+
+    if [ "${VERSION_AUTO_DEPLOY_DRY_RUN:-}" == "false" ]
+    then
+        git push --follow-tags
+    else
+        echo "RUNNING GIT PUSH DRY-RUN"
+        git push --follow-tags --dry-run
+        echo "RUNNING GIT PUSH DRY-RUN"
+    fi
+    echo
+    echo "Successfully pushed"
+}
+
+#-------------------------------------------------------------------------------
+
+if git tag --list | egrep -q "^$TAG_MASTER$"
 then
-    echo "Tag $VERSION_UPM already existed. Skipping the deployment"
-    exit 0
-fi
-
-echo ">> Backing up package at /package"
-mkdir -pv /package
-cp -fv \
-    $REPO_FOLDER/CHANGELOG.md \
-    $REPO_FOLDER/THIRD-PARTY-NOTICES.md \
-    $PACKAGE_FOLDER/.
-
-cp -r $PACKAGE_FOLDER/. /package/.
-echo
-
-echo ">> Checking out upm branch"
-git checkout upm --force
-echo
-
-echo ">> Replacing package"
-shopt -s dotglob
-GLOBIGNORE='.git' git rm \* -rf --ignore-unmatch
-git clean -dfx
-mv /package/* $REPO_FOLDER/.
-shopt -u dotglob
-git add .
-echo
-
-echo ">> Status"
-git status --short
-STATUS="$(git status --short)"
-echo
-
-if [ -z "$STATUS" ]
-then
-    echo "No changes to package in UPM branch. Will not create a new commit."
+    echo "Tag $TAG_MASTER already existed. Skipping the tagging on master branch"
 else
-    git commit -m "Json.NET Unity Converters $VERSION_UPM
+    tag_and_push $TAG_MASTER "Json.NET Unity Converters $VERSION_UPM
 
 Created by CircleCI job
 Build #$CIRCLE_BUILD_NUM
 $CIRCLE_BUILD_URL"
-    echo "Created commit '$(git log -n1 --format="%s")'"
 fi
-echo
 
-git tag $VERSION_UPM -m "Json.NET Unity Converters $VERSION_UPM
+#-------------------------------------------------------------------------------
+
+if git tag --list | egrep -q "^$TAG_UPM$"
+then
+    echo "Tag $TAG_UPM already existed. Skipping the deployment to upm branch"
+else
+
+    echo ">> Backing up package at /package"
+    mkdir -pv /package
+    cp -fv \
+        $REPO_FOLDER/CHANGELOG.md \
+        $REPO_FOLDER/THIRD-PARTY-NOTICES.md \
+        $PACKAGE_FOLDER/.
+
+    cp -r $PACKAGE_FOLDER/. /package/.
+    echo
+
+    echo ">> Checking out upm branch"
+    git checkout upm --force
+    echo
+
+    echo ">> Replacing package"
+    shopt -s dotglob
+    GLOBIGNORE='.git' git rm \* -rf --ignore-unmatch
+    git clean -dfx
+    mv /package/* $REPO_FOLDER/.
+    shopt -u dotglob
+    git add .
+    echo
+
+    echo ">> Status"
+    git status --short
+    STATUS="$(git status --short)"
+    echo
+
+    if [ -z "$STATUS" ]
+    then
+        echo "No changes to package in UPM branch. Will not create a new commit."
+    else
+        git commit -m "Json.NET Unity Converters $VERSION_UPM
+
+Based on commit $CIRCLE_SHA1
 
 Created by CircleCI job
 Build #$CIRCLE_BUILD_NUM
 $CIRCLE_BUILD_URL"
+        echo "Created commit '$(git log -n1 --format="%s")'"
+    fi
+    echo
 
-echo "Created tag '$(git tag -l $VERSION_UPM -n1)'"
+    tag_and_push $TAG_UPM "Json.NET Unity Converters $VERSION_UPM
 
-if [ "${VERSION_AUTO_DEPLOY_DRY_RUN:-}" == "false" ]
-then
-    git push --follow-tags
-else
-    echo "RUNNING GIT PUSH DRY-RUN"
-    git push --follow-tags --dry-run
-    echo "RUNNING GIT PUSH DRY-RUN"
+Based on commit $CIRCLE_SHA1
+
+Created by CircleCI job
+Build #$CIRCLE_BUILD_NUM
+$CIRCLE_BUILD_URL"
 fi
-echo
-echo "Successfully pushed"
